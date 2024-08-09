0d39f21d3265e86ffae4998e0f61e7096d2827ab
Yiosko <diegoalejandrogutierrez77@gmail.com>
2024-08-09 00:12:23 -0500

    Actualizacion cap 6
    se actualizaron los archivos del capitulo 6

--- Practicas/JS/Capitulo 6/codigo1.js
+++ Practicas/JS/Capitulo 6/codigo1.js
@@ -16,3 +16,53 @@ const inputNormal = document.querySelector(".input-normal");
 inputNormal.minLength = 8; //minimo 8 caracteres
 inputNormal.placeholder = "Matame" //mensajito del input}
 inputNormal.required = " "; //es necesario llenar este campo
+
+//Obtener ELEMENTOS
+
+const textCreate = document.querySelector(".text-create");
+document.write(`Ejemplo: ${textCreate.textContent}<br>`);
+
+const innerHTMLCreate = document.querySelector(".innerHTML-create");
+document.write(`Ejemplo: ${innerHTMLCreate.innerHTML} <br>`);
+
+//crear objetos
+
+const container = document.querySelector(".container-parent");
+const item = document.createElement("LI");
+
+document.write(`se crea el elemento li: ${item}`);
+
+//Pero si queremos generar un hijo dentro del nodo
+//_________________Manera objeto text
+// const childItem = document.createTextNode("ElementOfTheList");
+
+// item.appendChild(childItem);
+// console.log(item);
+
+//ingresar en el contenedor
+
+
+
+//__________Manera texto
+
+item.innerHTML = "ElementOfTheList";
+
+
+// forma no practica de realizar varios elementos dentro del container
+// for (let n = 0; n < 10 ; n++) {
+//     const item = document.createElement("LI");
+//     item.innerHTML = "ElementOfTheList";
+//     container.appendChild(item)
+// }
+
+//forma practica de hacerlo
+
+const fragmento = document.createDocumentFragment();
+
+for (let i = 0; i < 5; i++) {
+    const item = document.createElement("LI");
+    item.innerHTML = "ElementOfTheList";
+    fragmento.appendChild(item);
+}
+
+container.appendChild(fragmento);
